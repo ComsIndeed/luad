f931d928628a6136ca8dfe7d29e882755b327abc
ComsIndeed <enderdragonmaster8059@gmail.com>
2023-11-19 19:56:28 +0800

    Started building the front-end

--- luad/src/pages/Homepage.jsx
+++ luad/src/pages/Homepage.jsx
@@ -1,33 +1,45 @@
-import { Link } from "react-router-dom"
-import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-import { db } from "../config/firebase"
-import { collection, getDocs, doc } from "firebase/firestore"
+import { db } from "../config/firebase";
+import { collection, getDocs, doc } from "firebase/firestore";
 
-export function Homepage() {
-  const [articlePosts, setArticlePosts] = useState([])
-  
+function Header() {
+  return (
+    <>
+      <div className="header">
+        <div className="icon" />
+        <h1>Luad</h1>
+        <p>The collection of Potterians' talents, one click at a time</p>
+        <button>Learn More</button>
+      </div>
+    </>
+  );
+}
+
+function HomepageContent() {
+  const [articlePosts, setArticlePosts] = useState([]);
+
   const getArticlePosts = async () => {
     try {
       const data = await getDocs(collection(db, "posts-article"));
       const filteredData = data.docs.map((doc) => ({
         ...doc.data(),
         id: doc.id,
-      }))
-      setArticlePosts(filteredData)
-    } catch(err) {
-      console.error(err)
+      }));
+      setArticlePosts(filteredData);
+    } catch (err) {
+      console.error(err);
     }
-  }
+  };
 
-  useEffect(() => {getArticlePosts()}, [])
+  useEffect(() => {
+    getArticlePosts();
+  }, []);
 
   return (
     <>
-      <h1>The homepage</h1>
-
       <div className="content">
-
         {articlePosts.map((item) => {
           return (
             <div className="articlePost">
@@ -35,11 +47,19 @@ export function Homepage() {
               <blockquote> {item.author} </blockquote>
               <p> {item.content} </p>
             </div>
-          )
+          );
         })}
-
       </div>
-
     </>
-  )
+  );
+}
+
+export function Homepage() {
+  return (
+    <>
+      <Header />
+
+      <HomepageContent />
+    </>
+  );
 }
