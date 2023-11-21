7becc4c3cb3696c58e4b5396cc03d80aec4f3a99
ComsIndeed <enderdragonmaster8059@gmail.com>
2023-11-21 15:35:36 +0800

    Doing some tests

--- luad/src/App.jsx
+++ luad/src/App.jsx
@@ -1,22 +1,16 @@
 
 import { ReactDOM } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import { Homepage } from "./Pages/Homepage";
-import { LandingPage } from "./Pages/LandingPage";
-import { NavigationBar } from "./Components";
+// import { Homepage } from "./Pages/Homepage";
+// import { LandingPage } from "./Pages/LandingPage";
+// import { NavigationBar } from "./Components";
 
 
 export default function App() {
   return (
     <>
-    {/* <NavigationBar /> */}
-      <BrowserRouter basename={import.meta.env.DEV ? '/' : '/react-vite-gh-pages/'}>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/GetStarted" element={<LandingPage />} />
-        </Routes>
-      </BrowserRouter>
+      <h1>Just some basic HTML</h1>
     </>
   );
 }
