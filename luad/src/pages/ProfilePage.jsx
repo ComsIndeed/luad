c5783638f83ab56247c8956882f35063b503eea8
ComsIndeed <enderdragonmaster8059@gmail.com>
2023-11-25 00:08:27 +0800

    Got the accounts working

--- luad/src/pages/ProfilePage.jsx
+++ luad/src/pages/ProfilePage.jsx
@@ -1,36 +1,59 @@
-import { signOut } from "firebase/auth";
-import { auth, signInWithGoogle } from "../config/firebase";
+import { signInWithPopup, signOut } from "firebase/auth";
+import { auth, provider, signInWithGoogle } from "../config/firebase";
 import { useState, useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 
+function Pages(props) {
+  if (props.entry.error) {
+    return (
+      <>
+        <p>Error: {JSON.stringify(props.entry.error)} </p>
+      </>
+    );
+  }
+  if (props.entry.loading) {
+    return (
+      <>
+        <h3>Loading profile...</h3>
+      </>
+    );
+  }
+  if (props.entry.user) {
+    const user = props.entry.user;
+    return (
+      <>
+        <img src={user.photoURL} />
+        <h2> {user.displayName} </h2>
+        <button
+          onClick={() => {
+            signOut(auth);
+          }}
+        >
+          Signout
+        </button>
+      </>
+    );
+  }
+  return (
+    <>
+      <button
+        onClick={() => {
+          signInWithPopup(auth, provider);
+        }}
+      >
+        Login
+      </button>
+    </>
+  );
+}
+
 export function ProfilePage() {
   const [user, loading, error] = useAuthState(auth);
-  const signOutWithAuth = () => {
-    signOut(auth);
-  };
 
   return (
     <>
       <div className="profilePage">
-        <h1>Profile Page</h1>
-
-        <div className="controls">
-          {user ? (
-            <button
-              onClick={() => {
-                signOut(auth);
-              }}
-            >
-              Signout
-            </button>
-          ) : (
-            <button onClick={signInWithGoogle}>Attempt Signin</button>
-          )}
-        </div>
-
-        <div className="infoDiv">
-          {user ? <p> {JSON.stringify(user)} </p> : <p>User not found</p>}
-        </div>
+        <Pages entry={{ user, loading, error }} />
       </div>
     </>
   );
