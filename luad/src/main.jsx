706a0d24f19cc3cf03e28d3be7ca26c5658341d7
ComsIndeed <enderdragonmaster8059@gmail.com>
2023-11-28 23:42:47 +0800

    Reduced video file size

--- luad/src/main.jsx
+++ luad/src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './main.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./main.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
