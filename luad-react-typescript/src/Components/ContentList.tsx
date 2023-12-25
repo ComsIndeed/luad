9cc04e42e3b0e6dbaf0443facd5f7e731e8f44dc
ComsIndeed <enderdragonmaster8059@gmail.com>
2023-12-25 21:04:28 +0800

    Fixed all TS errors by more boilerplate :/

--- luad-react-typescript/src/Components/ContentList.tsx
+++ luad-react-typescript/src/Components/ContentList.tsx
@@ -1,11 +1,18 @@
-import React, { Suspense } from "react";
+import React, { Suspense, ReactNode } from "react";
 import { useEffect, useState } from "react";
-import { fetchFromFirestore } from "../Library/firestoreControls";
+import {
+  fetchFromFirestore,
+  FirestoreDocument,
+} from "../Library/firestoreUtils";
 import { Loading } from "../Reusables/Loading";
 import { Link } from "react-router-dom";
 import { paths } from "../Configuration/paths";
 
-function SuspendedDisplay({ children }) {
+interface SuspendedDisplayProps {
+  children: ReactNode;
+}
+
+function SuspendedDisplay({ children }: SuspendedDisplayProps) {
   return (
     <>
       <Suspense fallback={<Loading />}>{children}</Suspense>
@@ -13,34 +20,50 @@ function SuspendedDisplay({ children }) {
   );
 }
 
-function ContentGrid({ entries }) {
-  const ContentCard = ({ entry }) => {
-    return (
-      <>
-        <Link to={paths.posts + entry.id} className="ContentCard">
-          <img
-            className="ContentCard-thumbnail"
-            height={250}
-            src={entry.headerImage}
-            alt={`Thumbnail for the article: "${entry.title}"`}
-          />
-          <h2 className="ContentCard-title"> {entry.title} </h2>
-        </Link>
-      </>
-    );
-  };
+interface ContentCardProps {
+  entry: FirestoreDocument;
+}
 
+function ContentCard({ entry }: ContentCardProps) {
+  return (
+    <>
+      <Link to={paths.posts + entry.id} className="ContentCard">
+        <img
+          className="ContentCard-thumbnail"
+          height={250}
+          src={entry.headerImage}
+          alt={`Thumbnail for the article: "${entry.title}"`}
+        />
+        <h2 className="ContentCard-title"> {entry.title} </h2>
+      </Link>
+    </>
+  );
+}
+
+interface ContentGridProps {
+  entries: FirestoreDocument[];
+}
+
+function ContentGrid({ entries }: ContentGridProps) {
   return (
     <>
       <div className="ContentGrid">
         {entries.map((fetchedDocument) => {
-          return <ContentCard entry={fetchedDocument} />;
+          return (
+            <ContentCard key={fetchedDocument.id} entry={fetchedDocument} />
+          );
         })}
       </div>
     </>
   );
 }
-function ContentList({ entries }) {
+
+interface ContentListProps {
+  entries: FirestoreDocument[];
+}
+
+function ContentList({ entries }: ContentListProps) {
+  console.log(entries);
   return (
     <>
       <div className="ContentList"></div>
@@ -49,11 +72,14 @@ function ContentList({ entries }) {
 }
 
 export function Contents() {
-  const [collectionList, setCollectionList] = useState([]);
+  const [collectionList, setCollectionList] = useState<FirestoreDocument[]>([]);
   const [displayType, setDisplayType] = useState("grid");
 
   useEffect(() => {
     fetchFromFirestore("/content").then((returnedCollection) => {
+      if (!Array.isArray(returnedCollection)) {
+        throw new Error("This aint no collection");
+      }
       setCollectionList(returnedCollection);
     });
   }, []);
