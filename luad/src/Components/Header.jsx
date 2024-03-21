089a797287d786f01a9a049ca86f2faab49a15ee
ComsIndeed <Enderdragonmaster8059@gmail.com>
2024-03-22 02:51:31 +0800

    Lots of CSS Changes

--- luad/src/Components/Header.jsx
+++ luad/src/Components/Header.jsx
@@ -1,28 +1,93 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ThemeToggle from "../Reusables/ThemeToggle";
 import LUADIcon from "../Assets/luad.webp";
+import "animate.css";
 
-function HeaderScreen_Main() {
+function HeaderScreen_Main({ setScreenState, screenState }) {
   return (
     <>
-      <div className="HeaderScreen_Main">
+      <div className="HeaderScreen_Main ">
         <ThemeToggle />
         <img className="Header-icon" src={LUADIcon} alt="Luad Icon" />
         <h1 className="Header-title">LUAD</h1>
         <p className="Header-subtitle">
           The Potterian's Talents, one click at a time
         </p>
-        <button id="LearnMoreButton">Learn more</button>
+        <button
+          id="LearnMoreButton"
+          onClick={() => {
+            setScreenState("info");
+          }}
+        >
+          Learn more
+        </button>
+      </div>
+    </>
+  );
+}
+
+function HeaderScreen_Info() {
+  const [titleState, setTitleState] = useState("center");
+
+  const [visibleCards, setVisibleCards] = useState([]);
+
+  const ItemCards = [
+    <button className="ItemCard">
+      <h1 className="ItemCard-header"> TEST_HEADER 1 </h1>
+    </button>,
+    <button className="ItemCard">
+      <h1 className="ItemCard-header"> TEST_HEADER 2 </h1>
+    </button>,
+    <button className="ItemCard">
+      <h1 className="ItemCard-header"> TEST_HEADER 3 </h1>
+    </button>,
+    <button className="ItemCard">
+      <h1 className="ItemCard-header"> TEST_HEADER 4 </h1>
+    </button>,
+  ];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTitleState("top");
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="HeaderScreen_Info">
+        <h1
+          className={
+            "animate__animated animate__fadeIn Header-title " + titleState
+          }
+        >
+          Welcome to <span className="Header-title-LUAD">LUAD</span>
+        </h1>
+        <div className="options">
+          {ItemCards.map((item) => {
+            setTimeout(() => {
+              return item;
+            }, 500);
+          })}
+        </div>
       </div>
     </>
   );
 }
 
 export function Header() {
+  const [screenState, setScreenState] = useState("default");
+
   return (
     <>
-      <div className="Header">
-        <HeaderScreen_Main />
+      <div className={"Header " + screenState}>
+        {screenState === "default" && (
+          <HeaderScreen_Main
+            screenState={screenState}
+            setScreenState={setScreenState}
+          />
+        )}
+
+        {screenState === "info" && <HeaderScreen_Info />}
       </div>
     </>
   );
