39a0a8818e85739dfdd7b4ce736cc46fd7c0036e
Coms <125128463+ComsIndeed@users.noreply.github.com>
2024-03-23 20:58:47 +0800

    Update Homepage.jsx
    Commented pagination component out

--- luad/src/Pages/Homepage.jsx
+++ luad/src/Pages/Homepage.jsx
@@ -12,8 +12,8 @@ export function Homepage({ isAdmin }) {
         <HomepageNavigation isAdmin={isAdmin} />
         <Contents />
 
-        {/* Pagination is yet to be done. Just rework it at this point. */}
-        <Pagination />
+        {/* Pagination is yet to be done. Just rework it at this point.
+        <Pagination /> */}
       </div>
     </>
   );
