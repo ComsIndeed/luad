6d9701b6116ae9122270d51b9406a218c12fc79a
ComsIndeed <Enderdragonmaster8059@gmail.com>
2024-03-30 15:18:50 +0800

    Manually chunked out firebase, checking to see results

--- luad/vite.config.js
+++ luad/vite.config.js
@@ -4,4 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          firebase_auth: ["firebase/auth"],
+          firebase_firestore: ["firebase/firestore"],
+          firebase_storage: ["firebase/storage"],
+        },
+      },
+    },
+  },
 });
